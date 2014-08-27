4756bf54934c4a8795d4b240462402bcd6021dd2
rbaillard <rbaillard@sqli.com>
2014-08-27 11:29:37 +0200

    Delete done columns from ci_projects

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140805152100) do
+ActiveRecord::Schema.define(:version => 20140805150300) do
 
   create_table "actions", :force => true do |t|
     t.text     "action"
@@ -212,9 +212,6 @@ ActiveRecord::Schema.define(:version => 20140805152100) do
     t.text     "report"
     t.text     "previous_report"
     t.text     "svn_delivery_folder"
-    t.integer  "sqli_validation_done",             :default => 0
-    t.integer  "airbus_validation_done",           :default => 0
-    t.integer  "deployment_done",                  :default => 0
   end
 
   create_table "companies", :force => true do |t|
