23a8c015859136b61835782b908ae6a6dd33bc8f
eisq <rbaillard@sqli.com>
2015-01-21 16:14:00 +0100

    fix migrate

---
+++ db/migrate/20140805152100_add_column_done_to_ci_projects.rb
@@ -0,0 +1,13 @@
+class AddColumnDoneToCiProjects < ActiveRecord::Migration
+  def self.up
+  	add_column :ci_projects, :sqli_validation_done, :integer, :default=> 0
+  	add_column :ci_projects, :airbus_validation_done, :integer, :default=> 0
+  	add_column :ci_projects, :deployment_done, :integer, :default=> 0
+  end
+
+  def self.down
+  	remove_column :ci_projects, :sqli_validation_done
+  	remove_column :ci_projects, :airbus_validation_done
+  	remove_column :ci_projects, :deployment_done
+  end
+end
