2754a796499504a9142fd1134267839bf3c48d26
eisq <rbaillard@sqli.com>
2015-02-04 14:42:35 +0100

    Patch 1 - actions

--- app/controllers/context_chooser_controller.rb
+++ app/controllers/context_chooser_controller.rb
@@ -8,11 +8,9 @@ class ContextChooserController < ApplicationController
 
   def index
     now = DateTime.now
-  	@current_actions_from_user = Action.find(:all, :conditions=>["person_id=? and progress in('open','in_progress')", current_user.id], :order=>"due_date")
   	@actions      = Action.find(:all, :conditions=>["person_id=? and progress in('open','in_progress')", current_user.id], :order=>"due_date")
   	@action_48hr  = Action.find(:all, :conditions=>["person_id=? and progress in('open','in_progress') and updated_at >= ?", current_user.id, now - 48.hours], :order=>"due_date")
-  	@actions_accueil = Action.find(:all, :conditions=>["person_id=? and progress in('open','in_progress') and updated_at < ?", current_user.id, now - 48.hours], :order=>"due_date")
-  	#@recent_current_actions_from_user = @current_actions_from_user.select { |action| (Time.now - action.creation_date) < 72}
+  	@actions_old = Action.find(:all, :conditions=>["person_id=? and progress in('open','in_progress') and updated_at < ?", current_user.id, now - 48.hours], :order=>"due_date")
   end
 
 
