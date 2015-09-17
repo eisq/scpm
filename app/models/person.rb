2000286d10ddef38801242e058a0676d10c867c5
eisq <rbaillard@sqli.com>
2015-09-17 10:11:15 +0200

    test

--- app/models/person.rb
+++ app/models/person.rb
@@ -129,8 +129,8 @@ class Person < ActiveRecord::Base
       i = i + 1
     end
 
-    if i != 0
-      percent = (log_stop.balance - balance_init) / log_stop.initial
+    if i != 0 and log_stop.initial != 0
+      percent = ((log_stop.balance - balance_init) / log_stop.initial) * 100
       #raise "Percent: " + percent.to_s + " , balance_init: " + balance_init.to_s + " , balance_stop: " + log_stop.balance.to_s + " , log_stop_init: " + log_stop.initial.to_s
     end
 
