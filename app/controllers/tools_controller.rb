d707be2955d226d2dfb502389fb732729678be5c
eisq <rbaillard@sqli.com>
2016-01-04 15:16:45 +0100

    fix

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -853,7 +853,9 @@ class ToolsController < ApplicationController
     streams.each{ |s| 
       @streams_array << [s.name, s.id]
     }
-    requests          = Request.find(:all, :conditions=>["status != 'removed' and work_package IN (?) or work_package IN (?)", "%"+WORKPACKAGE_QS[0..1]+"%","%"+WORKPACKAGE_SPIDERS[0..1]+"%"], :order=>"request_id")
+    spiders_array = [WORKPACKAGE_SPIDERS[0], WORKPACKAGE_SPIDERS[1]]
+    qs_array = [WORKPACKAGE_QS[0], WORKPACKAGE_QS[1]]
+    requests          = Request.find(:all, :conditions=>["status != 'removed' and work_package IN (?) or work_package IN (?)", "%"+qs_array+"%","%"+spiders_array+"%"], :order=>"request_id")
     @requests_array   = [] 
     requests.each{ |r| 
       @requests_array << [r.request_id.to_s+" "+r.summary, r.id ]
