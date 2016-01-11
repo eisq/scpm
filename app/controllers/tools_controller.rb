4b175ef341ccd3b786daaff1331a938600aad80b
eisq <rbaillard@sqli.com>
2016-01-11 17:44:43 +0100

    fix

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -919,17 +919,9 @@ class ToolsController < ApplicationController
     end
 
     @table_spider_counter = table_spider_counter_temp.sort_by { |tsc| [tsc.historycounter.request_id, tsc.historycounter.action_date]}
-    
-#test
-    test_counter = 0
-    @table_spider_counter.each do |iter|
-      if iter.historycounter.request.id == 8485
-        test_counter = test_counter + 1
-        Rails.logger.info("%%%%%%%%%%%%%%%%%%%%% " + iter.historycounter.id.to_s)
-      end
+    @table_spider_counter.each do |tt|
+      Rails.logger.info("%%%%%%%%%%%%%%%%% : " + tt.spider_version.to_s)
     end
-    Rails.logger.info("%%%%%%%%%%%%%%%%%%%% finaly: " + test_counter.to_s )
-    #raise test_counter.to_s
 
     @qs_counter     = HistoryCounter.find(:all,:conditions=>[qs_condition],
                                           :joins => ["JOIN requests ON requests.id = history_counters.request_id", 
