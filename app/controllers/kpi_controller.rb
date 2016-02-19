75c77ca45970dfe9c9ead23347d3b2792ef81606
eisq <rbaillard@sqli.com>
2016-02-19 10:37:50 +0100

    fix

--- app/controllers/kpi_controller.rb
+++ app/controllers/kpi_controller.rb
@@ -15,20 +15,29 @@ class KpiController < ApplicationController
 
 		DeviationSpiderSetting.find(:all).each do |setting|
 			setting_info = Setting_info.new
-			setting_info.lifecycle = setting.deviation_reference.project.lifecycle
-			setting_info.workstream = setting.deviation_reference.project.workstream.name
-			setting_info.plm = setting.deviation_reference.project.suite_tag.name
-			setting_info.activity_name = setting.activity_name
-			setting_info.deliverable_name = setting.deliverable_name
-			setting_info.plan_to_do = setting.answer_1
-			@settings << setting_info
+			setting_info.plm = ""
+			setting_info.lifecycle = ""
+			if setting.deviation_spider_reference.project
+				setting_info.lifecycle = setting.deviation_spider_reference.project.lifecycle_object.name
+				setting_info.workstream = setting.deviation_spider_reference.project.workstream
+				if setting.deviation_spider_reference.project.suite_tag
+					setting_info.plm = setting.deviation_spider_reference.project.suite_tag.name
+				end
+				setting_info.activity_name = setting.activity_name
+				setting_info.deliverable_name = setting.deliverable_name
+				setting_info.plan_to_do = setting.answer_1
+				@settings << setting_info
+			end
 		end
 
 		SvtDeviationSpiderSetting.find(:all).each do |setting|
 			setting_info = Setting_info.new
-			setting_info.lifecycle = setting.deviation_reference.project.lifecycle
-			setting_info.workstream = setting.deviation_reference.project.workstream.name
-			setting_info.plm = setting.deviation_reference.project.suite_tag.name
+			setting_info.plm = ""
+			setting_info.lifecycle = setting.svt_deviation_spider_reference.project.lifecycle_object.name
+			setting_info.workstream = setting.svt_deviation_spider_reference.project.workstream
+			if setting.svt_deviation_spider_reference.project.suite_tag
+				setting_info.plm = setting.svt_deviation_spider_reference.project.suite_tag.name
+			end
 			setting_info.activity_name = setting.activity_name
 			setting_info.deliverable_name = setting.deliverable_name
 			setting_info.plan_to_do = setting.answer_1
@@ -37,9 +46,12 @@ class KpiController < ApplicationController
 
 		SvfDeviationSpiderSetting.find(:all).each do |setting|
 			setting_info = Setting_info.new
-			setting_info.lifecycle = setting.deviation_reference.project.lifecycle
-			setting_info.workstream = setting.deviation_reference.project.workstream.name
-			setting_info.plm = setting.deviation_reference.project.suite_tag.name
+			setting_info.plm = ""
+			setting_info.lifecycle = setting.svf_deviation_spider_reference.project.lifecycle_object.name
+			setting_info.workstream = setting.svf_deviation_spider_reference.project.workstream
+			if setting.svf_deviation_spider_reference.project.suite_tag
+				setting_info.plm = setting.svf_deviation_spider_reference.project.suite_tag.name
+			end
 			setting_info.activity_name = setting.activity_name
 			setting_info.deliverable_name = setting.deliverable_name
 			setting_info.plan_to_do = setting.answer_1
