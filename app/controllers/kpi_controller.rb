d11eb903b276eea073e7270d705c10320b45844b
eisq <rbaillard@sqli.com>
2016-02-24 09:22:33 +0100

    file for OM Adherence KPI

--- app/controllers/kpi_controller.rb
+++ app/controllers/kpi_controller.rb
@@ -5,7 +5,7 @@ class KpiController < ApplicationController
 	layout 'tools'
   	include WelcomeHelper
 
-	Setting_info    = Struct.new(:lifecycle, :workstream, :plm, :activity_name, :deliverable_name, :plan_to_do)
+	Setting_info    = Struct.new(:lifecycle, :workstream, :plm, :activity_name, :macro_activity_name, :deliverable_name, :plan_to_do)
 
 	def index
 	end
@@ -13,22 +13,22 @@ class KpiController < ApplicationController
 	def extract_list_deliverable
 		@settings = Array.new
 
-		DeviationSpiderSetting.find(:all).each do |setting|
-			setting_info = Setting_info.new
-			setting_info.plm = ""
-			setting_info.lifecycle = ""
-			if setting.deviation_spider_reference.project
-				setting_info.lifecycle = setting.deviation_spider_reference.project.lifecycle_object.name
-				setting_info.workstream = setting.deviation_spider_reference.project.workstream
-				if setting.deviation_spider_reference.project.suite_tag
-					setting_info.plm = setting.deviation_spider_reference.project.suite_tag.name
-				end
-				setting_info.activity_name = setting.activity_name
-				setting_info.deliverable_name = setting.deliverable_name
-				setting_info.plan_to_do = setting.answer_1
-				@settings << setting_info
-			end
-		end
+		#DeviationSpiderSetting.find(:all).each do |setting|
+		#	setting_info = Setting_info.new
+		#	setting_info.plm = ""
+		#	setting_info.lifecycle = ""
+		#	if setting.deviation_spider_reference.project
+		#		setting_info.lifecycle = setting.deviation_spider_reference.project.lifecycle_object.name
+		#		setting_info.workstream = setting.deviation_spider_reference.project.workstream
+		#		if setting.deviation_spider_reference.project.suite_tag
+		#			setting_info.plm = setting.deviation_spider_reference.project.suite_tag.name
+		#		end
+		#		setting_info.activity_name = setting.activity_name
+		#		setting_info.deliverable_name = setting.deliverable_name
+		#		setting_info.plan_to_do = setting.answer_1
+		#		@settings << setting_info
+		#	end
+		#end
 
 		SvtDeviationSpiderSetting.find(:all).each do |setting|
 			setting_info = Setting_info.new
@@ -39,24 +39,25 @@ class KpiController < ApplicationController
 				setting_info.plm = setting.svt_deviation_spider_reference.project.suite_tag.name
 			end
 			setting_info.activity_name = setting.activity_name
+			setting_info.macro_activity_name = setting.macro_activity_name
 			setting_info.deliverable_name = setting.deliverable_name
 			setting_info.plan_to_do = setting.answer_1
 			@settings << setting_info
 		end
 
-		SvfDeviationSpiderSetting.find(:all).each do |setting|
-			setting_info = Setting_info.new
-			setting_info.plm = ""
-			setting_info.lifecycle = setting.svf_deviation_spider_reference.project.lifecycle_object.name
-			setting_info.workstream = setting.svf_deviation_spider_reference.project.workstream
-			if setting.svf_deviation_spider_reference.project.suite_tag
-				setting_info.plm = setting.svf_deviation_spider_reference.project.suite_tag.name
-			end
-			setting_info.activity_name = setting.activity_name
-			setting_info.deliverable_name = setting.deliverable_name
-			setting_info.plan_to_do = setting.answer_1
-			@settings << setting_info
-		end
+		#SvfDeviationSpiderSetting.find(:all).each do |setting|
+		#	setting_info = Setting_info.new
+		#	setting_info.plm = ""
+		#	setting_info.lifecycle = setting.svf_deviation_spider_reference.project.lifecycle_object.name
+		#	setting_info.workstream = setting.svf_deviation_spider_reference.project.workstream
+		#	if setting.svf_deviation_spider_reference.project.suite_tag
+		#		setting_info.plm = setting.svf_deviation_spider_reference.project.suite_tag.name
+		#	end
+		#	setting_info.activity_name = setting.activity_name
+		#	setting_info.deliverable_name = setting.deliverable_name
+		#	setting_info.plan_to_do = setting.answer_1
+		#	@settings << setting_info
+		#end
 
 		if @settings.count > 0
 	        begin
