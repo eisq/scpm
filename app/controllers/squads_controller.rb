4da2d11ede9f6113fd0235cddb5a87e2e2fae62c
eisq <rbaillard@sqli.com>
2016-03-29 14:52:20 +0200

    fix

--- app/controllers/squads_controller.rb
+++ app/controllers/squads_controller.rb
@@ -6,7 +6,6 @@ class SquadsController < ApplicationController
   	if params[:squad]
   		@current_squad = Squad.find(:first, :conditions=>["id = ?", params[:squad]])
   	end
-
   	@squads = Array.new
   	 PersonSquad.find(:all, :conditions=>["person_id = ?", current_user.id], :order=> "squad_id").each do |person_squad|
   	 	squad = Squad.find(:first, :conditions=>["id = ?", person_squad.squad_id])
@@ -14,15 +13,19 @@ class SquadsController < ApplicationController
   	 		if !params[:squad]
   	 			@current_squad = squad
   	 		end
-  	 		@squads << squad
+  	 		#@squads << squad
   	 	end
-  	 end
+  	end
+    @squads = Squad.all
 
   	#Get current squad informations
   	@persons = Array.new
+    if !@current_squad
+      @current_squad = Squad.first
+    end
   	PersonSquad.find(:all, :conditions=>["squad_id = ?", @current_squad.id]).each do |person_squad|
       person = Person.find(:first, :conditions=>["id = ?", person_squad.person_id])
-      if person and person.is_transverse != 0 and has_left != 0
+      if person and person.is_transverse == 0 and person.has_left == 0
   		  @persons << person
       end
   	end
