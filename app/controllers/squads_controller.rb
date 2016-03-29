b9d18a4f56a7d35dd678ba5e6f056e01c7a43a67
eisq <rbaillard@sqli.com>
2016-03-29 15:32:37 +0200

    release 3 finalized

--- app/controllers/squads_controller.rb
+++ app/controllers/squads_controller.rb
@@ -30,11 +30,38 @@ class SquadsController < ApplicationController
       end
   	end
 
-
-
   	#PDC view
+    @workloads = Array.new
+    @totals_5_weeks = Array.new
+    @cap_totals_5_weeks = Array.new
+    @totals_3_months = Array.new
+    @cap_totals_3_months = Array.new
+    @avail_totals = Array.new
 
+    for p in @persons
+      if APP_CONFIG['workloads_add_by_project']
+        next if not p.has_workload_for_projects?(@project_ids)
+      end
+      w = Workload.new(p.id,[],'','', {:add_holidays=>true})
+      next if w.wl_lines.select{|l| l.wl_type != WL_LINE_HOLIDAYS}.size == 0 # do not display people with no lines at all
+      @workloads << w
+    end
+    @workloads = @workloads.sort_by {|w| [-w.person.is_virtual, w.next_month_percents, w.three_next_months_percents, w.person.name]}
+    size = @workloads.size
 
+    # next 5 weeks
+    @totals_5_weeks << (@workloads.inject(0) { |sum,w| sum += w.next_month_percents} / size).round
+    @cap_totals_5_weeks << (@workloads.inject(0) { |sum,w| sum += cap(w.next_month_percents)} / size).round
+    # next 3 months
+    @totals_3_months << (@workloads.inject(0) { |sum,w| sum += w.three_next_months_percents} / size).round
+    @cap_totals_3_months << (@workloads.inject(0) { |sum,w| sum += cap(w.three_next_months_percents)} / size).round
+    # next 8 weeks
+    @avail_totals << (@workloads.inject(0) { |sum,w| sum += w.sum_availability })
+
+  end
+
+  def cap(nb)
+    nb > 100 ? 100 : nb
   end
 
 end
