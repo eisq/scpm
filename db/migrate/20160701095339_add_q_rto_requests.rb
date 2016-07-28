143dea3439d309fcd01df7277bc3a5de0e724057
eisq <rbaillard@sqli.com>
2016-07-28 20:07:49 +0200

    bug on qr field fixed

--- db/migrate/20160701095339_add_q_rto_requests.rb
+++
@@ -1,9 +0,0 @@
-class AddQRtoRequests < ActiveRecord::Migration
-  def self.up
-  		add_column :requests, :QR, :string
-  end
-
-  def self.down
-  		remove_column :requests, :QR
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -513,6 +513,24 @@ ActiveRecord::Schema.define(:version => 20160701095339) do
     t.datetime "updated_at"
   end
 
+  create_table "lesson_collect_axes", :force => true do |t|
+    t.string "name"
+  end
+
+  create_table "lesson_collect_file_analyzes", :force => true do |t|
+    t.integer  "person_id"
+    t.integer  "lesson_collect_file_id"
+    t.string   "comment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "lesson_collect_file_downloads", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "lesson_collect_file_id"
+    t.datetime "download_date"
+  end
+
   create_table "lesson_collect_files", :force => true do |t|
     t.string   "pm"
     t.string   "qwr_sqr"
@@ -521,6 +539,21 @@ ActiveRecord::Schema.define(:version => 20160701095339) do
     t.string   "project_name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "lesson_collect_template_type_id"
+    t.integer  "request_id"
+    t.string   "filename"
+    t.string   "mt_qr"
+    t.text     "comment"
+    t.boolean  "is_archived",                     :default => false
+  end
+
+  create_table "lesson_collect_sub_axes", :force => true do |t|
+    t.string  "name"
+    t.integer "lesson_collect_axe_id"
+  end
+
+  create_table "lesson_collect_template_types", :force => true do |t|
+    t.string "name"
   end
 
   create_table "lesson_collects", :force => true do |t|
@@ -535,6 +568,16 @@ ActiveRecord::Schema.define(:version => 20160701095339) do
     t.string   "sub_axes"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "lesson_collect_axe_id"
+    t.integer  "lesson_collect_sub_axe_id"
+    t.string   "escalate_next_level"
+    t.string   "project_name"
+    t.boolean  "action_plan"
+    t.boolean  "already_exist"
+    t.string   "redundancy"
+    t.boolean  "selected"
+    t.string   "status"
+    t.string   "raised_in_dws_plm"
   end
 
   create_table "lifecycle_milestones", :force => true do |t|
@@ -600,6 +643,16 @@ ActiveRecord::Schema.define(:version => 20160701095339) do
     t.string   "reason_description"
     t.integer  "reason_one_id"
     t.boolean  "is_active"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "milestone_delay_reasons", :force => true do |t|
+    t.string   "reason_description"
+    t.integer  "level_of_reason"
+    t.integer  "reason_first_id_link"
+    t.integer  "reason_second_id_link"
+    t.integer  "reason_third_id_link"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -834,7 +887,7 @@ ActiveRecord::Schema.define(:version => 20160701095339) do
     t.integer  "tbp_project_id"
     t.boolean  "deviation_spider",     :default => false
     t.boolean  "deviation_spider_svt", :default => false
-    t.boolean  "deviation_spider_svf", :default => true
+    t.boolean  "deviation_spider_svf", :default => false
     t.boolean  "is_on_hold",           :default => false
   end
 
@@ -921,7 +974,7 @@ ActiveRecord::Schema.define(:version => 20160701095339) do
     t.integer  "stream_id"
     t.string   "is_stream",               :default => "No"
     t.string   "specific"
-    t.string   "QR"
+    t.string   "qr"
   end
 
   add_index "requests", ["project_id"], :name => "IDX_REQUESTS_ON_PROJECT_ID"
@@ -1569,9 +1622,9 @@ ActiveRecord::Schema.define(:version => 20160701095339) do
     t.string   "planned"
     t.string   "achieved"
     t.string   "comment"
-    t.boolean  "is_consolidated",              :default => false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "is_consolidated",              :limit => 1, :default => 0
   end
 
   create_table "svt_deviation_spider_references", :force => true do |t|
