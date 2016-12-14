1a2a351f6bc29d188f29bd2918fb1b66686164e7
eisq <rbaillard@sqli.com>
2016-12-14 06:19:23 +0100

    fix

--- db/migrate/20161206240000_add_column_pre_post_to_mdelay_records.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnPrePostToMdelayRecords < ActiveRecord::Migration
-  def self.up
-  		add_column :mdelay_records, :pre_post_gm_five, :string
-  end
-
-  def self.down
-  		remove_column :squads, :pre_post_gm_five
-  end
-end
